4840798b3d09b9c0503b7efa29a20920aab2449e
Void Main <voidmain1313113@gmail.com>
2013-04-20 22:45:20 +0800

    allow cross domain request

--- server.rb
+++ server.rb
@@ -3,6 +3,8 @@ require './attendee'
 require './github'
 
 get '/summary' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+
   drinkup_name = params['place'] + "-" + params['date']
   summary = []
   Github.get_sheets(drinkup_name).each do |sheet|
