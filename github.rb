43fb0c13249330d174c1fa24d46ff5a5f4a55987
Void Main <voidmain1313113@gmail.com>
2013-04-21 12:31:41 +0800

    add api-key for more users

--- github.rb
+++ github.rb
@@ -6,18 +6,21 @@ class Github
 
   def self.get_sheets branch_name
     sheet_folder_uri = "https://api.github.com/repos/secreek/drinkup/contents/sheets"
-    params = {:ref => branch_name}
+    params = JSON.load(open("github_key.json").read())
+    params["ref"] = branch_name
     return NetworkUtils.do_request_returning_json sheet_folder_uri, params
   end
 
   def self.get_raw_content branch_name, path
     content_uri = "https://raw.github.com/secreek/drinkup/#{branch_name}/#{path}"
-    return NetworkUtils.do_request_returning_json content_uri
+    params = JSON.load(open("github_key.json").read())
+    return NetworkUtils.do_request_returning_json content_uri, params
   end
 
   def self.get_user_info_from_id inner_id
     user_uri = "https://api.github.com/user/" + inner_id
-    return NetworkUtils.do_request_returning_json user_uri
+    params = JSON.load(open("github_key.json").read())
+    return NetworkUtils.do_request_returning_json user_uri, params
   end
 
 end
